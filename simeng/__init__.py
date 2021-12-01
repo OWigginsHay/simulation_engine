61223d97427e97c5a0d17af8b7e5988a2010dc7f
OWiggins-PC\OWiggins <oligames@live.co.uk>
2021-12-01 04:12:50 +0000

    test commit

---
+++ simeng/__init__.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simeng',
+    version='0.0.1',
+    packages=find_packages(include=['simeng', 'simeng.*']),
+    install_requires=[
+
+    ]
+)
